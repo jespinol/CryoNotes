0fc6239e3d2b5888b73908b5586f450fbccda4b6
jespinol <76534045+jespinol@users.noreply.github.com>
2022-01-10 20:19:35 -0500

    Initial commit

---
+++ src/main/java/com/jmel/demo/CryoNotesApplication.java
@@ -0,0 +1,13 @@
+package com.jmel.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryoNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryoNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jmel/demo/CryoNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmel.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryoNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
