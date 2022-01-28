c6333e0078222a48696fb6ab1ea02ac31000e0d8
jespinol <76534045+jespinol@users.noreply.github.com>
2022-01-28 15:04:56 -0500

    Additional code for sample class and controller

--- src/main/java/com/jmel/cryonotes/controllers/ComponentsController.java
+++ src/main/java/com/jmel/cryonotes/controllers/ComponentsController.java
@@ -1,6 +1,8 @@
 package com.jmel.cryonotes.controllers;
 
+import com.jmel.cryonotes.models.Sample;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,6 +11,13 @@ public class ComponentsController {
     @GetMapping("/components/samples_view")
     public String viewSamples() {
         return "/components/samples_view";
+    }
+
+    @GetMapping("/components/sample_add")
+    public String addSamples(Model model) {
+        model.addAttribute("sample", new Sample());
+
+        return "/comonents/sample_add";
     }
 }
 
--- src/main/java/com/jmel/cryonotes/models/Sample.java
+++ src/main/java/com/jmel/cryonotes/models/Sample.java
@@ -10,5 +10,77 @@ public class Sample {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column
+    private String sampleName;
 
+    @Column
+    private String sampleCategory;
+
+    @Column
+    private double molecularWeight;
+
+    @Column
+    private boolean isComplex;
+
+    @Column
+    private int stoichiometry;
+
+    @Column
+    private String comments;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSampleName() {
+        return sampleName;
+    }
+
+    public void setSampleName(String sampleName) {
+        this.sampleName = sampleName;
+    }
+
+    public String getSampleCategory() {
+        return sampleCategory;
+    }
+
+    public void setSampleCategory(String sampleCategory) {
+        this.sampleCategory = sampleCategory;
+    }
+
+    public double getMolecularWeight() {
+        return molecularWeight;
+    }
+
+    public void setMolecularWeight(double molecularWeight) {
+        this.molecularWeight = molecularWeight;
+    }
+
+    public boolean isComplex() {
+        return isComplex;
+    }
+
+    public void setComplex(boolean complex) {
+        isComplex = complex;
+    }
+
+    public int getStoichiometry() {
+        return stoichiometry;
+    }
+
+    public void setStoichiometry(int stoichiometry) {
+        this.stoichiometry = stoichiometry;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
 }
