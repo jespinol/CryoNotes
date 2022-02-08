5d3a00b525aeb21426a085439eb9b48097234e83
jespinol <76534045+jespinol@users.noreply.github.com>
2022-02-07 22:47:17 -0500

    Small changes to sign up page validation

--- src/main/java/com/jmel/cryonotes/controllers/SignInUpController.java
+++ src/main/java/com/jmel/cryonotes/controllers/SignInUpController.java
@@ -2,13 +2,18 @@ package com.jmel.cryonotes.controllers;
 
 import com.jmel.cryonotes.models.User;
 import com.jmel.cryonotes.models.data.UserRepository;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
 
 import javax.validation.Valid;
 
@@ -20,17 +25,17 @@ public class SignInUpController {
 
     @GetMapping("")
     public String viewStartPage() {
-        return "login/index";
+        return "/login/index";
     }
 
     @GetMapping("/register")
     public String showRegistrationForm(Model model) {
         model.addAttribute("user", new User());
-        return "login/signup_form";
+        return "/login/signup_form";
     }
 
     @PostMapping("/process_register")
-    public String processRegister(@Valid User user, BindingResult bindingResult) {
+    public String processRegister(@Valid User user, BindingResult bindingResult){
         if (bindingResult.hasErrors()) {
             return "/login/signup_form";
         }
@@ -38,8 +43,19 @@ public class SignInUpController {
         String encodedPassword = passwordEncoder.encode(user.getPassword());
         user.setPassword(encodedPassword);
         userRepository.save(user);
-        return "login/index";
+        return "/login/index";
     }
+
+//    @ExceptionHandler(ConstraintViolationException.class)
+//    public ResponseEntity<Object> handleConstraintViolation(ConstraintViolationException e, WebRequest request) {
+//        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+//    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public String handleConstraintViolation() {
+        return "redirect:/register";
+    }
+
 
     @GetMapping("/home")
     public String listUsers(Model model) {
