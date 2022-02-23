56396a4cb38a5ca230da93ee23ff2c7231bf40a9
jespinol <76534045+jespinol@users.noreply.github.com>
2022-02-23 09:36:18 -0500

    Fixed bug that generated improper redirect after attempting to view details of one or more entries following a search

--- src/main/java/com/jmel/cryonotes/controller/ScreeningsController.java
+++ src/main/java/com/jmel/cryonotes/controller/ScreeningsController.java
@@ -4,7 +4,6 @@ import com.jmel.cryonotes.model.Screening;
 import com.jmel.cryonotes.repository.ScreeningRepository;
 import com.jmel.cryonotes.service.TemplateVariables;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
